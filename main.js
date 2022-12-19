e7d1ff970fea42df3e38ffbef18023773d872781
erodarob <jbhauerstein@gmail.com>
2022-12-19 14:06:40 -0500

    set up debug window

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
 const { app, BrowserWindow, ipcMain } = require("electron");
 const path = require("path");
+const { debug } = require("./debug");
 
-let win;
+let mainWin, debugWin;
 
 const createWindow = () => {
-  win = new BrowserWindow({
+  mainWin = new BrowserWindow({
     width: 1200,
     height: 600,
     resizable: false,
@@ -15,13 +16,32 @@ const createWindow = () => {
     },
   });
 
-  win.loadFile("src/index.html");
+  mainWin.loadFile("src/index.html");
 
-  win.webContents.openDevTools({ mode: "detach" });
+  mainWin.webContents.openDevTools({ mode: "detach" });
+};
+
+const createDebug = () => {
+  debugWin = new BrowserWindow({
+    width: 600,
+    height: 400,
+    resizable: false,
+    autoHideMenuBar: true,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
+
+  debug.setWin(debugWin);
+
+  debugWin.loadFile("src/debug/debug.html");
+
+  debugWin.webContents.openDevTools({ mode: "detach" });
 };
 
 app.whenReady().then(() => {
-  createWindow();
+  //createWindow();
+  createDebug();
 
   app.on("activate", () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -51,3 +71,8 @@ ipcMain.on("reload", async (event, args) => {
 ipcMain.on("dev-tools", (event, args) => {
   win.webContents.openDevTools({ mode: "detach" });
 });
+
+let counter = 0;
+setInterval(() => {
+  debug.println("test" + counter++);
+}, 100);
--- preload.js
+++ preload.js
@@ -8,4 +8,25 @@ const APP = {
   devTools: () => ipcRenderer.send("dev-tools"),
 };
 
+let con = document.getElementById("debug-console");
+let scrolled = false;
+
+ipcRenderer.on("print", (event, message) => {
+  if (!con) {
+    con = document.getElementById("debug-console");
+  }
+  if (con) {
+    document.addEventListener("scroll", () => {
+      scrolled = true;
+    });
+    con.textContent += message;
+    if (
+      scrolled &&
+      window.scrollY >= con.scrollHeight - window.innerHeight - 20
+    )
+      scrolled = false;
+    if (!scrolled) window.scrollTo(0, con.scrollHeight);
+  }
+});
+
 contextBridge.exposeInMainWorld("app", APP);
