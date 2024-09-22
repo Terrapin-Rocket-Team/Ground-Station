79c653e00060dbe03a1a9ebc27615aadad45dd90
Ramy Kaddouri <myramkadd@gmail.com>
2024-09-22 16:05:07 -0400

    initial work should be done

--- serial/LinuxSerialPort.hpp
+++ serial/LinuxSerialPort.hpp
@@ -1,15 +1,20 @@
+#pragma once
+
 #include "SerialPort.hpp"
+#include <termios.h>
 class LinuxSerialPort : public SerialPort {
 private:
-  int portNumber;
+  int portHandle;
+  bool connected;
+  struct termios backup;
 
 public:
   LinuxSerialPort(const char *portName);
   ~LinuxSerialPort();
 
-  virtual int readSerialPort(void *buffer, unsigned int buf_size) = 0;
-  virtual bool writeSerialPort(void *buffer, unsigned int buf_size) = 0;
-  virtual bool writeSerialPort(int data, unsigned int buf_size) = 0;
-  virtual bool isConnected() = 0;
-  virtual void closeSerial() = 0;
+  int readSerialPort(void *buffer, unsigned int buf_size) override;
+  bool writeSerialPort(void *buffer, unsigned int buf_size) override;
+  bool writeSerialPort(int data, unsigned int buf_size) override;
+  bool isConnected() override;
+  void closeSerial() override;
 };
