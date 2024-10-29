3e8f02d7eb529084e72396837ba92b655ef23f74
Ramy Kaddouri <myramkadd@gmail.com>
2024-10-28 21:52:03 -0400

    some build script edits

--- video/build.sh
+++ video/build.sh
@@ -15,6 +15,12 @@ if ! [ -d dav1d-1.4.2 ] ; then
     rm dav1d-1.4.2.tar.xz
 fi
 
-mkdir -p dav1d-1.4.2/build && cd dav1d-1.4.2/build && meson setup -Denable_tools=false -Denable_tests=false --default-library=static .. && ninja && ninja install
-cd ../../ffmpeg-7.0.1 && ./configure --enable-libdav1d  --disable-avdevice --extra-cflags="-I/mingw64/include" --extra-ldflags="-L/mingw64/lib"
-make -j $CORES
+if ! type ffmpeg &> /dev/null ; then
+    echo "ERROR: Unable to locate ffmpeg, will build and install"
+    if ! type dav1d &> /dev/null ; then
+        echo "ERROR: Unable to locate dav1d, will build and install"
+        mkdir -p dav1d-1.4.2/build && cd dav1d-1.4.2/build && meson setup -Denable_tools=false -Denable_tests=false --default-library=static .. && ninja && ninja install
+        cd ../../ffmpeg-7.0.1 && ./configure --enable-libdav1d  --disable-avdevice --extra-cflags="-I/mingw64/include" --extra-ldflags="-L/mingw64/lib"
+        make -j $CORES
+    fi
+fi
