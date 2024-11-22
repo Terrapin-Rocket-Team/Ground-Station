65b3651e33e9e4c772ba8fbad46647d0cd94d388
jhauerst <jhauerst@terpmail.umd.edu>
2024-11-21 21:25:45 -0500

    I give up, manually fixing merge

--- serial/WinNamedPipe.cpp
+++ serial/WinNamedPipe.cpp
@@ -11,7 +11,7 @@ WinNamedPipe::WinNamedPipe(const char *name, bool create) : NamedPipe(name)
 {
     if (create)
     {
-        hPipe = CreateNamedPipe(TEXT(name), PIPE_ACCESS_INBOUND,
+        hPipe = CreateNamedPipe(TEXT(name), PIPE_ACCESS_DUPLEX,
                                 PIPE_TYPE_BYTE | PIPE_READMODE_BYTE | PIPE_WAIT, 1,
                                 1024 * 16, 1024 * 16, NMPWAIT_USE_DEFAULT_WAIT, NULL);
     }
@@ -34,16 +34,41 @@ WinNamedPipe::WinNamedPipe(const char *name, bool create) : NamedPipe(name)
     }
 }
 
+int WinNamedPipe::readStr(char *buffer, int bufferSize)
+{
+    unsigned long int read = 1;
+    unsigned int count = 0;
+    while (count < bufferSize && read > 0)
+    {
+        read = 0;
+        ReadFile(hPipe, buffer + count, 1, &read, NULL);
+        if (buffer[count] == '\n')
+        {
+            buffer[count] = '\0';
+            break;
+        }
+        count += read;
+    }
+    return count;
+}
+
+int WinNamedPipe::writeStr(const char *buffer)
+{
+    unsigned long int written = 0;
+    WriteFile(hPipe, buffer, strlen(buffer), &written, NULL);
+    return written;
+}
+
 int WinNamedPipe::read(void *buffer, int bufferSize)
 {
-    unsigned int read = 0;
+    unsigned long int read = 0;
     ReadFile(hPipe, buffer, bufferSize, &read, NULL);
     return read;
 }
 
 int WinNamedPipe::write(const void *buffer, int bufferSize)
 {
-    unsigned int written = 0;
+    unsigned long int written = 0;
     WriteFile(hPipe, buffer, bufferSize, &written, NULL);
     return written;
 }
@@ -52,6 +77,8 @@ WinNamedPipe::~WinNamedPipe()
 {
     if (hPipe != NULL)
     {
+        FlushFileBuffers(hPipe);
+        // DisconnectNamedPipe(hPipe);
         CloseHandle(hPipe);
     }
 }
