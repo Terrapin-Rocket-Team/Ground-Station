80be594b8e75185e75275c2826bd76be0f00835e
jhauerst <jhauerst@terpmail.umd.edu>
2024-11-21 21:13:37 -0500

    Revert "prototype new layout"
    This reverts commit f090b9d7790aeb53a8ccda8e5e17467bea50b66e.

--- src/index_new.js
+++ src/index_new.js
@@ -1,14 +1,13 @@
 window.onload = () => {
-  // "global" vars
   let fullscreened = false;
-  let portsInUse = [];
-
-  /// top bar
-
+  // let fullscreenCallbacks = [];
   //app control button listeners
   document.getElementById("fullscreen").addEventListener("click", () => {
     fullscreened = !fullscreened;
     api.fullscreen("main", fullscreened);
+    // fullscreenCallbacks.forEach((callback) => {
+    //   callback();
+    // });
   });
   document.getElementById("reload").addEventListener("click", () => {
     api.reload("main");
@@ -19,15 +18,20 @@ window.onload = () => {
   document.getElementById("close").addEventListener("click", () => {
     api.close("main");
   });
+  // document
+  //   .getElementById("radio-command-open")
+  //   .addEventListener("click", () => {
+  //     api.openCommand();
+  //   });
 
+  let portsInUse = [];
   //custom dropdown setup
-  const setupDropdown = (idPrefix, callback, reload) => {
-    const drop = document.getElementById(idPrefix + "-drop");
-    const options = document.getElementById(idPrefix + "-options");
-    if (!reload) callback(idPrefix);
+  const setupDropdown = (idPrefix) => {
     document
       .getElementById(idPrefix + "-drop")
       .addEventListener("click", () => {
+        const drop = document.getElementById(idPrefix + "-drop");
+        const options = document.getElementById(idPrefix + "-options");
         if (drop.classList.contains("active")) {
           options.style.display = "none";
           document
@@ -35,7 +39,7 @@ window.onload = () => {
             .setAttribute("src", "./images/arrow_right.svg");
         } else {
           options.style.display = "block";
-          if (reload) callback(idPrefix);
+          getAvailPorts(idPrefix);
           document
             .getElementById(idPrefix + "-arrow")
             .setAttribute("src", "./images/arrow_down.svg");
@@ -89,147 +93,14 @@ window.onload = () => {
     });
   };
 
-  setupDropdown("serial", getAvailPorts, true);
-
-  /// sidebar
-
-  const setupSidebar = (buttons, visualContainer) => {
-    let visuals = visualContainer.querySelectorAll("div.visual");
-    for (let i = 0; i < buttons.length; i++) {
-      buttons[i].addEventListener("click", () => {
-        for (let j = 0; j < visuals.length; j++) {
-          visuals[j].classList.remove("active");
-        }
-        for (let j = 0; j < buttons.length; j++) {
-          buttons[j].classList.remove("active");
-        }
-        let visualId = buttons[i].id.split("-")[0] + "-wrapper";
-        document.getElementById(visualId).classList.add("active");
-        buttons[i].classList.add("active");
-      });
-    }
-  };
-
-  const visButtonsTop = document.getElementsByClassName("vis-button-top");
-  const visButtonsBottom = document.getElementsByClassName("vis-button-bottom");
-  const topVisual = document.getElementById("visual-top");
-  const bottomVisual = document.getElementById("visual-bottom");
-
-  setupSidebar(visButtonsTop, topVisual);
-  setupSidebar(visButtonsBottom, bottomVisual);
-
-  const setupStaticOptions = (idPrefix, optionsList, clickCallback) => {
-    const options = document.getElementById(idPrefix + "-options");
-    while (options.childElementCount > 0) {
-      options.removeChild(options.firstChild);
-    }
-    const selected = document.getElementById(idPrefix + "-selected");
-
-    optionsList.forEach((option) => {
-      const span = document.createElement("SPAN");
-      span.className = idPrefix;
-      span.textContent = option;
-      span.addEventListener("click", () => {
-        selected.textContent = option;
-        if (clickCallback) clickCallback(option);
-      });
-      options.appendChild(span);
-    });
-  };
-
-  const getVideoLayouts = (idPrefix) => {
-    const layoutOptions = ["Full", "Partial", "Telemetry Only"];
-
-    setupStaticOptions(idPrefix, layoutOptions);
-  };
-
-  const getVideo0Displays = (idPrefix) => {
-    const video0Options = ["Input 0", "Input 1", "Charts", "None"];
-
-    setupStaticOptions(idPrefix, video0Options);
-  };
-  const getVideo1Displays = (idPrefix) => {
-    const video1Options = ["Input 0", "Input 1", "Charts", "None"];
-
-    setupStaticOptions(idPrefix, video1Options);
-  };
-
-  setupDropdown("video-layout", getVideoLayouts, false);
-  setupDropdown("video-0", getVideo0Displays, false);
-  setupDropdown("video-1", getVideo1Displays, false);
-
-  /// radio/commands
-
-  //adds commands to custom dropdown
-  const getCommands = (idPrefix) => {
-    const commandList = [
-      "Pi Power On",
-      "Pi Start Video",
-      "Record Launch Data",
-      "Restart Pi",
-    ];
-
-    const commandSyntax = [
-      "PPO: <time> <H/M/S>",
-      "PSV: <time> <H/M/S>",
-      "RLD: <record?>",
-      "RP: N/A",
-    ];
-
-    const commandCallback = (option) => {
-      let index = commandList.findIndex((command) => {
-        return command === option;
-      });
-      document.getElementById("command-syntax").textContent =
-        commandSyntax[index];
-      document.getElementById("command-args").value =
-        commandSyntax[index].split(" ")[0] + " ";
-    };
-
-    setupStaticOptions(idPrefix, commandList, commandCallback);
-  };
-
-  setupDropdown("command", getCommands, false);
-
-  const commandArgs = document.getElementById("command-args");
-  const previousCommands = document.getElementById("previous-commands");
-
-  document.getElementById("reset-command").addEventListener("click", () => {
-    document.getElementById("command-syntax").textContent =
-      "No command selected";
-    commandArgs.value = "";
-    document.getElementById("command-selected").textContent = "Select Command";
-  });
-
-  document.addEventListener("click", () => {
-    document.getElementById("confirm-send").classList.add("inactive");
-  });
-
-  document.getElementById("send-command").addEventListener("click", (e) => {
-    e.stopPropagation();
-    document.getElementById("confirm-send").classList.toggle("inactive");
-  });
-
-  document.getElementById("confirm-send").addEventListener("click", () => {
-    let command = commandArgs.value;
-    document.getElementById("command-syntax").textContent =
-      "No command selected";
-    commandArgs.value = "";
-    document.getElementById("command-selected").textContent = "Select Command";
-    const span = document.createElement("SPAN");
-    span.className = "previous-command";
-    span.textContent = command;
-    previousCommands.appendChild(span);
-    // add code to send command to backend here
-  });
-
-  /// middle/data display
+  setupDropdown("serial");
 
   const resizeGauges = () => {
+    console.log("Here");
     //set gauge.js gauge sizing
     let gauges = document.getElementsByClassName("gauge");
 
-    let size = document.getElementById("telem-1").offsetHeight * 0.35;
+    let size = document.getElementById("telem-1").offsetWidth * 0.7;
 
     let numGauges = gauges.length;
     for (let i = 0; i < numGauges; i++) {
@@ -246,11 +117,4 @@ window.onload = () => {
       }, 10);
     }
   });
-
-  // map
-  buildMap("map");
-
-  // charts
-  let altG = createChart("alt-graph", "Altitude", "s", "ft", 1, 1);
-  let spdG = createChart("spd-graph", "Speed", "s", "ft/s", 1, 1);
 };
