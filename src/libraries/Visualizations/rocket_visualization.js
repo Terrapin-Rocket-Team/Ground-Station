8ab2b871fd2202b27786cf68786c128710d7ab17
IsarUslu <110565260+IsarUslu@users.noreply.github.com>
2026-03-07 15:50:29 -0500

    added position lerp for rocket visualization

--- src/libraries/Visualizations/rocket_visualization.js
+++ src/libraries/Visualizations/rocket_visualization.js
@@ -29,6 +29,12 @@ function mountRocketScene() {
   const geometry = new THREE.BoxGeometry(2, 2, 2);
   const material = new THREE.MeshBasicMaterial({ color: "white" });
   const cube = new THREE.Mesh(geometry, material);
+
+  // necessary rotational information for the cube
+  const targetPosition = new THREE.Vector3(0, 0, 0);
+  const targetRotation = new THREE.Euler(0, 0, 0, "XYZ");
+  const tempEuler = new THREE.Euler(0, 0, 0, "XYZ");
+
   scene.add(cube);
 
   const renderer = new THREE.WebGLRenderer({ antialias: true });
@@ -37,11 +43,46 @@ function mountRocketScene() {
 
   mount.appendChild(renderer.domElement);
 
+  let startAlt = null;
+  window.api.on("data", (telem) => {
+    if (!telem) {
+      console.log("error in recieving telemetry");
+      return;
+    }
+
+    const altitude = Number(telem.altitude ?? 0); // numerical quntity
+    const orientation = telem.orientation ?? [0, 0, 0]; // vectorial quantity?
+
+    if (startAlt == null) {startAlt = altitude;}
+
+    const y_change = (altitude - startAlt) * 0.05; // movement rate
+    targetPosition.set(0, y_change, 0);
+
+    targetRotation.set(
+      THREE.MathUtils.degToRad(Number(orientation[0] ?? 0)),
+      THREE.MathUtils.degToRad(Number(orientation[1] ?? 0)),
+      THREE.MathUtils.degToRad(Number(orientation[2] ?? 0)),
+      "XYZ"
+    );
+  });
+
+
   function animate() {
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cube.position.lerp(targetPosition, 0.15);
+  
+    cube.rotation.x = THREE.MathUtils.lerp(cube.rotation.x, targetRotation.x, 0.15);
+    cube.rotation.y = THREE.MathUtils.lerp(cube.rotation.y, targetRotation.y, 0.15);
+    cube.rotation.z = THREE.MathUtils.lerp(cube.rotation.z, targetRotation.z, 0.15);
+
+    // move the camera w/ cube
+    camera.position.copy(cube.position).add(new THREE.Vector3(0, 3, 10)); // offset camera position
+    camera.lookAt(cube.position);
+
     renderer.render(scene, camera);
     requestAnimationFrame(animate);
+
+    console.log(cube.rotation);
+    console.log(cube.position);
   }
   animate();
 
